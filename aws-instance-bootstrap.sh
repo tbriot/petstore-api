c71d484f32b41dc259c84f3c82197d84f8a4c897
Thomas Briot <thomas.briot82@gmail.com>
2018-10-02 18:08:45 -0400

    Update aws-instance-bootstrap.sh

--- aws-instance-bootstrap.sh
+++ aws-instance-bootstrap.sh
@@ -1,4 +1,5 @@
 sudo yum -y update
-sudo yum -y install git python3 python-pip
+sudo yum -y install git python36 python-pip
 sudo git clone https://github.com/tbriot/petstore-api.git
-sudo pip install -r requirements.txt
+sudo pip install -r ./petstore-api/requirements.txt
+python3 ./petstore-api/petstore_api.py
