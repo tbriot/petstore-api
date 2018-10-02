f260cb7cd44041fe44921f7e296a3920ef147e0f
Thomas <thomas.briot82@gmail.com>
2018-10-02 18:29:36 -0400

    fixed tabs issue, replaced by spaces

--- petstore_api.py
+++ petstore_api.py
@@ -7,31 +7,31 @@ api = Api(app)
 class Pets(Resource):
     def get(self):
         return [
-			{
-				"id": 1,
-				"type": "dog",
-				"price": 249.99
-			},
-			{
-				"id": 2,
-				"type": "cat",
-				"price": 124.99
-			},
-			{
-				"id": 3,
-				"type": "fish",
-				"price": 0.99
-			}
-		]
+        {
+            "id": 1,
+            "type": "dog",
+            "price": 249.99
+         },
+         {
+            "id": 2,
+            "type": "cat",
+            "price": 124.99
+         },
+         {
+            "id": 3,
+            "type": "fish",
+            "price": 0.99
+         }
+      ]
 
 class Pet(Resource):
     def get(self, pet_id):
         if pet_id == 1:
-			return {"id": 1, "type": "dog",	"price": 249.99}
-        if pet_id == 2:		
-			return {"id": 2, "type": "cat",	"price": 124.99}
+            return {"id": 1, "type": "dog",   "price": 249.99}
+        if pet_id == 2:      
+            return {"id": 2, "type": "cat",   "price": 124.99}
         else:
-			return {"id": 3, "type": "fish", "price": 0.99}
+            return {"id": 3, "type": "fish", "price": 0.99}
 
 api.add_resource(Pets, '/api/pets')
 api.add_resource(Pet, '/api/pet/<int:pet_id>')
