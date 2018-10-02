57f8e65d643dc96d8463eb02838bea6256222ce5
Thomas Briot <thomas.briot82@gmail.com>
2018-10-02 18:09:05 -0400

    Update petstore_api.py

--- petstore_api.py
+++ petstore_api.py
@@ -26,13 +26,13 @@ class Pets(Resource):
 
 class Pet(Resource):
     def get(self, pet_id):
-        if pet_id == 1:		
+        if pet_id == 1:
 			return {"id": 1, "type": "dog",	"price": 249.99}
         if pet_id == 2:		
 			return {"id": 2, "type": "cat",	"price": 124.99}
-        else:		
-			return {"id": 3, "type": "fish",	"price": 0.99}
-		
+        else:
+			return {"id": 3, "type": "fish", "price": 0.99}
+
 api.add_resource(Pets, '/api/pets')
 api.add_resource(Pet, '/api/pet/<int:pet_id>')
 
